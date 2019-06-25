e397ddbd4584ff92794019bb6ccc8a57351719e2
newstreetpunk <newstreetpunk@yandex.ru>
2019-06-25 23:50:45 +0500

    Главный экран-десктоп

--- js/common.js
+++ js/common.js
@@ -1,50 +1,6 @@
 $(function() {
 
-	setTimeout("$('.main-bg-1').show();", 650);
-  setTimeout("$('.main-content-block').css('left', '0');", 600);
-	setTimeout("$('.main-bg-2').addClass('vis');", 600);
-	setTimeout("$('.main-bg-3').addClass('vis');", 650);
-
-	/*$('.main').parallax({
-    'elements': [
-    {
-      'selector': 'section.main',
-      'properties': {
-        'x': {
-          'background-position-x': {
-            'initial': 0,
-            'multiplier': 0.1
-          }
-        }
-      }
-    },
-    {
-      'selector': 'div.main-bg-3',
-      'properties': {
-        'x': {
-          'background-position-x': {
-            'initial': 50,
-            'multiplier': 0.05,
-            'unit': '%'
-          }
-        }
-      }
-    },
-    {
-      'selector': 'div.main-bg-2',
-      'properties': {
-        'x': {
-          'background-position-x': {
-            'initial': 50,
-            'multiplier': 0.05
-          }
-        }
-      }
-    }
-    ]
-});*/
-
-
+  setTimeout("$('.main-content-block .sa-btn').css('left', '0');", 2000);
 
 	//удаление атрибута 'style'
 	$(window).scroll(function(){
