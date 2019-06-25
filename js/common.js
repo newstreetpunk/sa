680ead47cdd37e5e086671d6caf24f95cbbb6bb6
newstreetpunk <newstreetpunk@yandex.ru>
2019-06-25 19:03:04 +0500

    Главный экран-начало

--- js/common.js
+++ js/common.js
@@ -1,5 +1,50 @@
 $(function() {
 
+	setTimeout("$('.main-bg-1').show();", 1000);
+	setTimeout("$('.main-bg-2').addClass('vis');", 1300);
+	setTimeout("$('.main-bg-3').addClass('vis');", 1400);
+
+	/*$('.main').parallax({
+    'elements': [
+    {
+      'selector': 'section.main',
+      'properties': {
+        'x': {
+          'background-position-x': {
+            'initial': 0,
+            'multiplier': 0.1
+          }
+        }
+      }
+    },
+    {
+      'selector': 'div.main-bg-3',
+      'properties': {
+        'x': {
+          'background-position-x': {
+            'initial': 50,
+            'multiplier': 0.05,
+            'unit': '%'
+          }
+        }
+      }
+    },
+    {
+      'selector': 'div.main-bg-2',
+      'properties': {
+        'x': {
+          'background-position-x': {
+            'initial': 50,
+            'multiplier': 0.05
+          }
+        }
+      }
+    }
+    ]
+});*/
+
+
+
 	//удаление атрибута 'style'
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 50){
@@ -10,3 +55,4 @@ $(function() {
 	});
 
 });
+
