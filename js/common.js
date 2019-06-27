034aebb13663800ed32a9d2ab6bdfebda0982acd
newstreetpunk <newstreetpunk@yandex.ru>
2019-06-27 18:39:38 +0500

    234

--- js/common.js
+++ js/common.js
@@ -30,6 +30,39 @@ $(function() {
 	nextArrow: '<button class="slick-next"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve">	<path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225 c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>'
   });
 
+  $('.video-popup').magnificPopup({
+  	disableOn: 700,
+  	mainClass: 'mfp-fade',
+  	removalDelay: 160,
+  	preloader: true,
+
+  	fixedContentPos: true,
+  	type: 'iframe',
+  	iframe: {
+  		markup: '<div class="mfp-iframe-scaler">'+
+  		'<div class="mfp-close"></div>'+
+  		'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+  		'</div>',
+  		patterns: {
+  			youtube: {
+  				index: 'youtube.com/', 
+  				id: 'v=',
+  				src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+  			},
+  			vimeo: {
+  				index: 'vimeo.com/',
+  				id: '/',
+  				src: '//player.vimeo.com/video/%id%?autoplay=1'
+  			},
+  			gmaps: {
+  				index: '//maps.google.',
+  				src: '%id%&output=embed'
+  			}
+  		},
+  		srcAction: 'iframe_src', 
+  	}
+  });
+
 	//удаление атрибута 'style'
 	$(window).scroll(function(){
 		if($(this).scrollTop() > 50){
