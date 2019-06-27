86c5fbb7a96d3f7dd4a9a8191ccc189d8a555ac2
newstreetpunk <newstreetpunk@yandex.ru>
2019-06-27 11:39:58 +0500

    234

--- js/common.js
+++ js/common.js
@@ -1,6 +1,27 @@
 $(function() {
 
-  setTimeout("$('.main-content-block .sa-btn').css('left', '0');", 100);
+  //setTimeout("$('.main-content-block .sa-btn').css('left', '0');", 100);
+
+  $('.menu-btn').click(function(){
+	$(this).toggleClass('active');
+  });
+
+  if ($(window).width() > 768) {
+
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+		if (prevScrollpos > currentScrollPos) {
+			document.getElementById("header").setAttribute(
+				"style", "top: 0; background-color: #fff; box-shadow: 0 3px 40px rgba(0,0,0,.3);");
+		} else {
+			document.getElementById("header").setAttribute(
+				"style", "top: -95px; background-color: #fff; box-shadow: 0 3px 40px transparent;");
+		}
+		prevScrollpos = currentScrollPos;
+	}
+
+  }
 
 	//удаление атрибута 'style'
 	$(window).scroll(function(){
@@ -9,10 +30,6 @@ $(function() {
 		}else{
 			$('.header').removeAttr('style');
 		}
-	});
-
-	$('.menu-btn').click(function(){
-		$(this).toggleClass('active');
 	});
 
 });
